e99e2a9db7f4eb0c23f8ef7105cfcd4ad36d5b09
Jie Yuan <jyuan@cs.columbia.edu>
2015-09-03 16:11:04 -0400

    added confidence interval calculation (this is 50% speculation - will have to return to this later)

--- ersa/chisquare.py
+++ ersa/chisquare.py
@@ -1,7 +1,10 @@
 from scipy import stats
 import math
 
-
-def likelihoodRatioTest(Lr, Ln, df=2):
+#confidence interval? get the inverse cdf at p=ci,p=1-ci
+def likelihoodRatioTest(Lr, Ln, df=2, ci=0.05):
     ratio = -2*math.log(Lr) + 2*math.log(Ln)
-    return 1-stats.chi2.cdf(ratio, df)
+
+    lowerCI = stats.chi2.ppf(ci,2)
+    upperCI = stats.chi2.ppf(1-ci,2)
+    return 1-stats.chi2.cdf(ratio, df), lowerCI, upperCI
