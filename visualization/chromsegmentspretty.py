110ca803fb452036e6341282862b2ac2c3766b37
Jie Yuan <jyuan@cs.columbia.edu>
2015-09-15 17:00:16 -0400

    Segments that occur at the ends of chromosomes now also have rounded edges.

--- visualization/chromsegmentspretty.py
+++ visualization/chromsegmentspretty.py
@@ -1,4 +1,5 @@
 import os
+import math
 from PIL import Image, ImageDraw, ImageFont
 from visualization.chromsegments import *
 
@@ -8,14 +9,25 @@ def draw_rounded_rectangle(input, x1, y1, x2, y2, fillcolor = 'lightblue'):
     rounded_radius = total_width / 2
 
     draw = ImageDraw.Draw(input)
-    draw.rectangle((x1 + rounded_radius, y1, x2 - rounded_radius, y2), fill = fillcolor)
-    draw.ellipse((x1, y1, x1 + 2 * rounded_radius, y2), fill = fillcolor)
-    draw.ellipse((x2 - 2 * rounded_radius, y1, x2, y2), fill = fillcolor)
+    draw.rectangle((x1, y1 + 2, x2, y2 - 2), fill = fillcolor)
+    #draw.rectangle((x1 + rounded_radius, y1, x2 - rounded_radius, y2), fill = fillcolor)
+    #draw.ellipse((x1, y1, x1 + 2 * rounded_radius, y2), fill = fillcolor)
+    #draw.ellipse((x2 - 2 * rounded_radius, y1, x2, y2), fill = fillcolor)
 
 def draw_chromosome(input, x1, y1, x2, y2, centromere_frac = 0.5, fillcolor = 'lightblue'):
     cent_x = centromere_frac * (x2 - x1)
     draw_rounded_rectangle(input, x1, y1, cent_x, y2, fillcolor)
     draw_rounded_rectangle(input, cent_x, y1, x2, y2, fillcolor)
+
+def round_edges(input, x1, y1, x2, y2, centromere_frac = 0.5, fillcolor = 'white'):
+    draw = ImageDraw.Draw(input)
+    radius = (y2 - y1) / 2
+    for i in range(0, int(y2 - y1)):
+        line_len = radius - math.sqrt(max(2 * radius * i - i * i, 0))
+        if line_len >= 1:
+            draw.line((x1, y1 + i, x1 + line_len, y1 + i), fill = fillcolor)
+            draw.line((centromere_frac * (x2 - x1) - line_len, y1 + i, centromere_frac * (x2 - x1) + line_len, y1 + i), fill = fillcolor)
+            draw.line((x2 - line_len, y1 + i, x2, y1 + i), fill = fillcolor)
 
 def create_pretty_chrom_plot(pair_dict, chrom_dict, xdim = 1024, ydim = 1024, pair = "unspecified"):
     # select pair if unspecified
@@ -47,6 +59,7 @@ def create_pretty_chrom_plot(pair_dict, chrom_dict, xdim = 1024, ydim = 1024, pa
         if chrom_dict[chrom][0] > longest_chrom:
             longest_chrom = chrom_dict[chrom][0]
 
+    print(pair)
     for chrom in range(1, num_autosomes + 1):
         x1 = 10
         x2 = x1 + chrom_dict[chrom][0] / longest_chrom * chrom_longest_len
@@ -68,8 +81,11 @@ def create_pretty_chrom_plot(pair_dict, chrom_dict, xdim = 1024, ydim = 1024, pa
             #segs.append((seg[1], seg[2] - seg[1])) #start, width
             seg_start = x1 + seg[1] / total_len * (x2 - x1)
             seg_end = x1 + seg[2] / total_len * (x2 - x1)
-            draw.rectangle((seg_start, y1, seg_end, y2), outline = 'blue')
 
+
+             # make this rounded at the edges when required
+            draw_rounded_rectangle(input, seg_start, y1, seg_end, y2, 'blue')
+        round_edges(input, x1, y1, x2, y2, cent_frac)
     input.show()
 
 def pretty_parse_and_plot(path, t, h):
