a5ad1011d2376bf92b58594c370bcb1dc4fb6cf7
Richard Munoz <rmunoz@cs.columbia.edu>
2015-09-24 13:08:15 -0400

    init package setup file

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = "Implementation of Huff et. al. (2011) Estimation of Recent Shared Ancestry"
+LONG_DESCRIPTION = DESCRIPTION
+NAME = "ersa"
+AUTHOR = "Richard Munoz, Jie Yuan, Yaniv Erlich"
+AUTHOR_EMAIL = "rmunoz@columbia.edu, jyuan@columbia.edu, yaniv@cs.columbia.edu"
+MAINTAINER = "Richard Munoz"
+MAINTAINER_EMAIL = "rmunoz@columbia.edu"
+DOWNLOAD_URL = 'http://github.com/rmunoz12/ersa'
+LICENSE = 'GNU GPL v3'
+
+VERSION = '1.0'
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      url=DOWNLOAD_URL,
+      download_url=DOWNLOAD_URL,
+      license=LICENSE,
+      packages=find_packages(),
+      test_suite='ersa.tests',
+      entry_points={
+          'console_scripts': [
+              'ersa = ersa:main',
+          ],
+      },
+      install_requires=['scipy', 'SQLAlchemy', 'SQLAlchemy_Utils',
+                        'inflect', 'pytest'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Programming Language :: Python :: 3.4',
+                   'License :: OSI Approved :: GNU GPL v3',
+                   'Operating System :: OS Independent',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics']
+      )
